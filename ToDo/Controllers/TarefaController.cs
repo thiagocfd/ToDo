8115c5f478d9367758b1e077a2d668f8415cddfe
Thiago Dourado <thiagocfdourado@gmail.com>
2021-03-16 20:28:43 -0300

    Inicio da criacao do CRUD (view Index)

---
+++ ToDo/Controllers/TarefaController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ToDo.Models;
+
+namespace ToDo.Controllers
+{
+    public class TarefaController : Controller
+    {
+        private static IList<Tarefa> tarefas =
+            new List<Tarefa>()
+            {
+                new Tarefa()
+                {
+                    ToDo = "Estudar .NET",
+                    TarefaID = 1
+                }
+            };
+      
+        public IActionResult Index()
+        {
+            return View(tarefas.OrderBy(i => i.TarefaID));
+        }
+    }
+}
---
+++ ToDo/Models/Tarefa.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDo.Models
+{
+    public class Tarefa
+    {
+        public string ToDo { get; set; }
+        public long TarefaID { get; set; }
+    }
+}
